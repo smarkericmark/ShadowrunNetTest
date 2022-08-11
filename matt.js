8a411ac0b008d030a776f70ec44a975742f83688
smarkericmark <52010110+smarkericmark@users.noreply.github.com>
2022-08-10 20:22:18 -0400

    Add files via upload

---
+++ matt.js
@@ -0,0 +1,125 @@
+///Button controls
+
+
+function clickHide() {
+	//document.getElementById("aboutBox").innerHTML = "While working as a Client Service Advisor, I directly aided a corporate client in recovering over $100,000 in stolen funds after the CEO's email was compromised. That incident drew my interest towards Information Security. That interest became a hobby and then a passion. In 2020 I decided to realign my career towards tech while leveraging self-learning, traditional education, and 5+ years of corporate finance experience.";
+	//document.getElementById("aboutIntroBlurb").element.className = "hidden";
+	document.getElementById("B01").classList.add('hidden');
+	document.getElementById("B02").classList.add('hidden');
+	document.getElementById("B03").classList.add('hidden');
+	document.getElementById("B04").classList.add('hidden');
+	document.getElementById("B05").classList.add('hidden');
+	document.getElementById("B06").classList.add('hidden');
+	document.getElementById("B07").classList.add('hidden');
+	document.getElementById("B08").classList.add('hidden');
+	document.getElementById("B09").classList.add('hidden');
+	document.getElementById("B10").classList.add('hidden');
+	document.getElementById("B09").classList.add('hidden');
+	document.getElementById("B10").classList.add('hidden');
+	document.getElementById("B12").classList.add('hidden');
+	document.getElementById("B11").classList.add('hidden');
+	document.getElementById("B12").classList.add('hidden');
+	document.getElementById("backButton").classList.remove('hidden');
+	document.getElementById("backButton").classList.add('aboutButton');
+
+	
+
+	
+}
+
+function clickShow() {
+	//document.getElementById("aboutBox").innerHTML = "While working as a Client Service Advisor, I directly aided a corporate client in recovering over $100,000 in stolen funds after the CEO's email was compromised. That incident drew my interest towards Information Security. That interest became a hobby and then a passion. In 2020 I decided to realign my career towards tech while leveraging self-learning, traditional education, and 5+ years of corporate finance experience.";
+	//document.getElementById("aboutIntroBlurb").element.className = "hidden";
+	
+	document.getElementById("B01").classList.remove('hidden');
+	document.getElementById("B02").classList.remove('hidden');
+	document.getElementById("B03").classList.remove('hidden');
+	document.getElementById("B04").classList.remove('hidden');
+	document.getElementById("B05").classList.remove('hidden');
+	document.getElementById("B06").classList.remove('hidden');
+	document.getElementById("B07").classList.remove('hidden');
+	document.getElementById("B08").classList.remove('hidden');
+	document.getElementById("B09").classList.remove('hidden');
+	document.getElementById("B10").classList.remove('hidden');
+	document.getElementById("B11").classList.remove('hidden');
+	document.getElementById("B12").classList.remove('hidden');
+
+	
+	document.getElementById("backButton").classList.add('hidden');
+
+	document.getElementById("File01").classList.add('hidden');
+	document.getElementById("File02").classList.add('hidden');
+	document.getElementById("File03").classList.add('hidden');
+	document.getElementById("File04").classList.add('hidden');
+	document.getElementById("File05").classList.add('hidden');
+	document.getElementById("File06").classList.add('hidden');
+	document.getElementById("File07").classList.add('hidden');
+	document.getElementById("File08").classList.add('hidden');
+	document.getElementById("File09").classList.add('hidden');
+	document.getElementById("File10").classList.add('hidden');
+	document.getElementById("File11").classList.add('hidden');
+	document.getElementById("File12").classList.add('hidden');
+
+	
+}
+
+
+function getFile01() {
+	document.getElementById("File01").classList.remove('hidden');
+	document.getElementById("File01").classList.add('p');
+}
+
+function getFile02() {
+	document.getElementById("File02").classList.remove('hidden');
+	document.getElementById("File02").classList.add('p');
+}
+
+function getFile03() {
+	document.getElementById("File03").classList.remove('hidden');
+	document.getElementById("File03").classList.add('p');
+}
+
+function getFile04() {
+	document.getElementById("File04").classList.remove('hidden');
+	document.getElementById("File04").classList.add('p');
+}
+
+function getFile05() {
+	document.getElementById("File05").classList.remove('hidden');
+	document.getElementById("File05").classList.add('p');
+}
+
+function getFile06() {
+	document.getElementById("File06").classList.remove('hidden');
+	document.getElementById("File06").classList.add('p');
+}
+
+function getFile07() {
+	document.getElementById("File07").classList.remove('hidden');
+	document.getElementById("File07").classList.add('p');
+}
+
+function getFile08() {
+	document.getElementById("File08").classList.remove('hidden');
+	document.getElementById("File08").classList.add('p');
+}
+
+function getFile09() {
+	document.getElementById("File09").classList.remove('hidden');
+	document.getElementById("File09").classList.add('p');
+}
+
+function getFile10() {
+	document.getElementById("File10").classList.remove('hidden');
+	document.getElementById("File10").classList.add('p');
+}
+
+function getFile11() {
+	document.getElementById("File11").classList.remove('hidden');
+	document.getElementById("File11").classList.add('p');
+}
+
+function getFile12() {
+	document.getElementById("File12").classList.remove('hidden');
+	document.getElementById("File12").classList.add('p');
+}
